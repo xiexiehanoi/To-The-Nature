ee663d8a870ba96d7ea1e404b977f313ce7b15b7
GaleRhee <jun5hero@naver.com>
2023-12-18 21:35:01 +0900

    [add] plistmapper, plistdao

--- src/main/java/nature/plist/PListDao.java
+++ src/main/java/nature/plist/PListDao.java
@@ -1,5 +1,6 @@
 package nature.plist;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +14,23 @@ public class PListDao {
 	private SqlSession session;
 	
 	private String nameSpace="nature.plist.PListDao.";
+	
+	public int getTotalCount() 
+	{
+		return session.selectOne(nameSpace+"totalCountOfSite");
+	}
+	
+	public List<PListDto> getSearchSites(String field,String word)
+	{
+		//word 에 검색 단어가 안 들어 있을 경우 null 값을 보내야 where 문이 실행 안된다
+		word=word==null || word.length()==0?null:word;
+		
+		Map<String, String> map=new HashMap<String, String>();
+		map.put("searchfield", field);
+		map.put("searchword", word);
+		
+		return session.selectList(nameSpace+"selectAllSites", map);
+	}
 	
 	public List<Map<String, Object>> getsearchcamping(Map<String, Object> plistParam) 
 	{
